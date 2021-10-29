2d94e048edd8faabbfb4a2ff4429ef75e4e78343
wangzhichao <wangzhichao03@tojoy.com>
2021-10-29 14:13:29 +0800

    first commit

---
+++ mini-rpc-core/src/main/java/cn/minirpc/core/minirpccore/MiniRpcCoreApplication.java
@@ -0,0 +1,13 @@
+package cn.minirpc.core.minirpccore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniRpcCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniRpcCoreApplication.class, args);
+    }
+
+}
---
+++ mini-rpc-monitor/src/test/java/cn/minirpc/monitor/MiniRpcMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.minirpc.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniRpcMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
